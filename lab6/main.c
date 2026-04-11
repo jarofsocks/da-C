9e327bfd2c3cb4cc7775f9ced63e7fa1deb81284
Казюта Степан <jarofsocks@stud.vvsu.ru>
2026-04-11 13:18:07 +1000

    zvc

--- lab6/main.c
+++ lab6/main.c
@@ -1,36 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
+int main() {
 
-
-void main() {
-	
-	int i, j;
-	char string[79];
-	FILE *f1 = fopen("data.txt","r");
-	FILE *f2 = fopen("newdata.txt", "w");
-	
-	while (fgets(string, sizeof(string), f1) != NULL) {
-		printf("%s", string);
-		if (string[-2] >= 8){
-			fprintf(f2, string);
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
+	setlocale(LC_ALL, ".1251");    
+    FILE *f1, *f2;
+    f1 = fopen("text.txt", "r");
+    f2 = fopen("newtext.txt", "a");
+    
+    int i, flag;
+    
+    int yr;
+    
+    char str[79], str1[79];
+    
+    char *point;
+    
+    
+    while (fgets(str, 79, f1) != NULL) {
+    	flag = 0;
+    	if (str[0] == "Ï") {
+    		flag = 1;
+		} 
+    	strcpy(str1, str);
+    	point = strtok(str," ");
+        for (i = 0; i < 3; i++) {
+            point = strtok(NULL," ");
+        }
+        printf("\n");
+        yr = atoi(point);
+        if ((yr > 1980) && (flag == 1)){
+            fprintf(f2, str1);
+            printf("%s", str1);
+        }
+    }
+    
+    fclose(f1);
+    fclose(f2);
+    
+    printf("\n\ndone");
+    
+    return 0;
 }
-
-
-
-
-
-
-
-
 
