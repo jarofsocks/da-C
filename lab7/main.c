e0139417b31c1a4a3790b1a5b26a83e83b00a8fb
Казюта Степан <jarofsocks@stud.vvsu.ru>
2026-05-16 13:23:46 +1000

    BVV

--- lab7/main.c
+++ lab7/main.c
@@ -3,15 +3,44 @@
 #include <string.h>
 #include <math.h>
 
+typedef struct {
+	char name[79];
+	char surname[79];
+    int yrofbirth;
+} humen;
 
+int i, j;
 
+int numoflines;
+
+int copy(humen *list1, humen *list2) {
+    for (i = 0; i < numoflines; i++) {
+        list2[i] = list1[i];
+    }
+    return 0;
+}
+
+int sort(humen *list1) {
+    humen memel;
+	for (j=0; j<numoflines-1; j++) {
+	    for (i=0; i<numoflines-j-1; i++) {
+	        if (list1[i+1].yrofbirth < list1[i].yrofbirth) {
+	            memel = list1[i];
+	            list1[i] = list1[i+1];
+	            list1[i+1] = memel;
+	        }
+	    }
+	    
+	}
+	    return 0;
+}
 int main() {
 	
 	FILE *f1;
 	
 	f1 = fopen("text.txt","r");
 	
-	int numoflines;
+
 	
 	char str[79];
 	
@@ -25,58 +54,19 @@ int main() {
 
 	f1 = fopen("text.txt","r");
 	
-	typedef struct {
-	        char name[79];
-	        char surname[79];
-	        int yrofbirth;
-	} humen;
+
 	    
 	humen hlist1[numoflines], hlist2[numoflines];
 	    
-	int i, j;
+
 	
-	int copy(humen *list1, humen *list2) {
-	    for (i = 0; i < numoflines; i++) {
-	        list2[i] = list1[i];
-	    }
-	    return 0;
-	}
 	
-	int sort(humen *list1) {
-	    humen memel;
-		for (j=0; j<numoflines-1; j++) {
-		    for (i=0; i<numoflines-j-1; i++) {
-		        if (list1[i+1].yrofbirth < list1[i].yrofbirth) {
-		            memel = list1[i];
-		            list1[i] = list1[i+1];
-		            list1[i+1] = memel;
-		        }
-		    }
-		    
-		}
-		    return 0;
-	}
     
-	int test = 0;
-    
-    if (test == 1) {
-        for (i = 0; i < 4; i++) {
-            humen newhumen = {
-            "goidin",
-            "ivanov",
-            2026 - i
-            };
-            hlist1[i] = newhumen;
-        }
-    } else {
-		i = 0;
-        while (i < numoflines) {
-        	fscanf(f1, "%s, %s, %d", &hlist1[i].name, &hlist1[i].surname, &hlist1[i].yrofbirth);
-        	printf("%s", hlist1[i].name);
-        	i++;
-        	}
-        	
-		}
+	
+	i = 0;
+    while (fscanf(f1, "%s %s %d", hlist1[i].name, hlist1[i].surname, &hlist1[i].yrofbirth) != EOF) {
+    	i++;
+    }
         
     
     
