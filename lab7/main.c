92d3a066006d90288f437cfb68f25f384893187d
Казюта Степан <jarofsocks@stud.vvsu.ru>
2026-05-16 12:07:19 +1000

    jkjk

--- lab7/main.c
+++ lab7/main.c
@@ -1,55 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-
-	
-int i, j;
-
-typedef struct humen {
-	int birth_yr;
-	char name[10];
-	char surname[10];
+typedef struct {
+        char name[79];
+        char surname[79];
+        int yrofbirth;
 } humen;
+    
+humen hlist1[4], hlist2[4];
+    
+int i, j;
+    
+int syrofbirth;
+    
+char sname[79], ssurname[79];
 
-void del_element(int element, humen *inp_arr) {
-	int length = sizeof(inp_arr) / sizeof(inp_arr[0]);
-	for (i = element; i < length-1; i++) {
-		inp_arr[i] = inp_arr[i+1];
+int copy(humen *list1, humen *list2) {
+    for (i = 0; i < 4; i++) {
+        list2[i] = list1[i];
+    }
+    return 0;
+}
+
+int sort(humen *list1) {
+    humen memel;
+	for (j=0; j<4; j++) {
+	    for (i=0; i<3; i++) {
+	        if (list1[i+1].yrofbirth < list1[i].yrofbirth) {
+	            memel = list1[i];
+	            list1[i] = list1[i+1];
+	            list1[i+1] = memel;
+	        }
+	    }
+	    
 	}
-	inp_arr[length] = ;
-}
-
-void sort(int *inp_arr, int *out_arr) {
-	int min, max;
-	min = 99999999999;
-	
-	for (i = 0; i < 4; i++) {
-		if (inp_arr[i] < min) {
-			min = inp_arr[i];
-		} 	
-	} 
-}
+	    return 0;
+	}
 
 int main() {
-	
-	int by;
-	char nm[10];
-	char snm[10];
-	
-	humen humen_arr1[4];
-	humen humen_arr2[4];
-	
-	for (i = 0; i < 4; i++) {
-		scanf("%d", &by);
-		scanf("%s", &nm);	
-		scanf("%s", &snm);
-
-		humen_arr1[i].birth_yr=by;
-		strcpy(humen_arr1[i].name, nm);
-		strcpy(humen_arr1[i].surname, snm);
-	
-		printf("%d %s %s \n", humen_arr1[i].birth_yr, humen_arr1[i].name, humen_arr1[i].surname);
-	}
-	return 0;
+    int test = 1;
+    
+    if (test == 1) {
+        for (i = 0; i < 4; i++) {
+            humen newhumen = {
+            "goidin",
+            "ivanov",
+            2026 - i
+            };
+            hlist1[i] = newhumen;
+        }
+    } else {
+        for (i = 0; i < 4; i++) {
+            scanf("%s %s %d", hlist1[i].name, hlist1[i].surname, &hlist1[i].yrofbirth);
+        }
+        
+    }
+    
+    
+    
+    
+    copy(hlist1, hlist2);
+    
+    sort(hlist2);
+    
+    printf("inp\n");
+    for (i = 0; i < 4; i++) {
+        printf("%s, %s, %d \n",hlist1[i].name, hlist1[i].surname, hlist1[i].yrofbirth);
+    }
+    
+    printf("\nout\n");
+    for (i = 0; i < 4; i++) {
+        printf("%s, %s, %d \n",hlist2[i].name, hlist2[i].surname, hlist2[i].yrofbirth);
+    }
+    
+    printf("\ndone");
+    
+    return 0;
 }
